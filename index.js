5a5ade87cc79c4433fbe21c0096bd1f1720d857b
unknown <bhattaraiankit2212@gmail.com>
2024-07-27 17:44:41 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express');
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('Siddhartha.com')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>please login at chai aur code')
+})
+
+app.get('/youtube', (req, res) => {
+    res.send("<h2>Chai aur code</h2>")
+
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+}) 
+
+
